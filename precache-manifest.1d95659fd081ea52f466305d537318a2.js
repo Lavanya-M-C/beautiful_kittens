b8abc62276187f31f7318b8d475ccfb995805a9f
root <mc.lavanya@mahindra.com>
2020-05-06 11:51:07 +0530

    Updates

---
+++ precache-manifest.1d95659fd081ea52f466305d537318a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fcb66fa425a950436367f87be5375a",
+    "url": "/beautiful_kittens/index.html"
+  },
+  {
+    "revision": "5b54b1e4f4b3ed537872",
+    "url": "/beautiful_kittens/static/css/main.1ab4c729.chunk.css"
+  },
+  {
+    "revision": "db467ac49d280d3602ad",
+    "url": "/beautiful_kittens/static/js/2.04765a99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/beautiful_kittens/static/js/2.04765a99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b54b1e4f4b3ed537872",
+    "url": "/beautiful_kittens/static/js/main.45870d58.chunk.js"
+  },
+  {
+    "revision": "ce322167df9c88e44652",
+    "url": "/beautiful_kittens/static/js/runtime-main.13a1589c.js"
+  }
+]);
